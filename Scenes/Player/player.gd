922c056b25c15e2c6ea4fa8a88e7af32ddb96dc9
Filipe Augusto <filipecordeiro213@gmail.com>
2025-10-05 11:14:24 -0300

    Player Moviment

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,6 +1,16 @@
 extends CharacterBody2D
 
 
+class_name Player
+
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+const JUMP_SPEED: float = -310.0 
+const RUN_SPEED: float = 100.0
+
+
 var _gravity: float = ProjectSettings.get('physics/2d/default_gravity')
 
 
@@ -13,11 +23,20 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 	
-	if is_on_floor():
+	if is_on_floor() and Input.is_action_just_pressed('jump') == true:
 		jump(delta)
+	
+	run()
 	
 	
 func jump(delta: float) -> void:
 	if Input.is_action_just_pressed('jump') == true:
-			velocity.y = -350.0	
+			velocity.y = JUMP_SPEED
 	
+
+func run() -> void:
+	var movement = Input.get_axis('left', 'right')
+	velocity.x = RUN_SPEED * movement
+	
+	if is_equal_approx(velocity.x, 0.0) == false:
+		sprite_2d.flip_h = velocity.x < 0
