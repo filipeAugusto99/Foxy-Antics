ec8678d2868efde2eab333a2142e5a605c93cb5b
Filipe Augusto <filipecordeiro213@gmail.com>
2025-10-19 10:59:05 -0300

    Bullets

---
+++ Scenes/LevelBase/LevelBase.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+const ENEMY_BULLET = preload("res://Scenes/BaseBullet/EnemyBullet.tscn")
+const PLAYER_BULLET = preload("res://Scenes/BaseBullet/PlayerBullet.tscn")
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("test") == true:
+		var b = PLAYER_BULLET.instantiate()
+		add_child(b)
+		
