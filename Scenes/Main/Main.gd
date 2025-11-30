454b1b350cf0357d4a2f0ba0d73248486dd6ea3f
Filipe Augusto <filipecordeiro213@gmail.com>
2025-11-30 11:10:01 -0300

    Scenes/HighScoreDisplay

--- Scenes/Main/Main.gd
+++ Scenes/Main/Main.gd
@@ -1,12 +1,22 @@
 extends Control
 
+const HIGHSCORE_DISPLAY = preload("res://Scenes/HigherScoreDisplay/HighScoreDisplay.tscn")
+
+
+@onready var grid_container: GridContainer = $MarginContainer/GridContainer
+
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot") == true:
 		GameManager.load_next_level()
 
 
-
-# Called when the node enters the scene tree for the first time.
+func set_scores() -> void:
+	for score: HighScore in GameManager.high_scores.get_scores_list():
+		var hsd: HighScoreDisplayItem = HIGHSCORE_DISPLAY.instantiate()
+		hsd.setup(score)
+		grid_container.add_child(hsd)
+		
+		
 func _ready() -> void:
-	pass # Replace with function body.
+	set_scores()
