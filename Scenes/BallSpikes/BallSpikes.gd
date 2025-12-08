29aae332e7cfd68381257973266ab7a2c8ffe5e4
Filipe Augusto <filipecordeiro213@gmail.com>
2025-12-08 11:42:46 -0300

    Ball Spikes

---
+++ Scenes/BallSpikes/BallSpikes.gd
@@ -0,0 +1,15 @@
+extends PathFollow2D
+
+@export var speed: float = 50.0
+@export var spin_speed: float = 360
+
+
+
+
+
+
+
+func _process(delta: float) -> void:
+	progress += speed * delta
+	rotation_degrees += spin_speed * delta
+	
