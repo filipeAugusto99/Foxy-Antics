b0786ba5c09c090a017fc46bc162f8576dcd19b4
Filipe Augusto <filipecordeiro213@gmail.com>
2025-12-08 15:15:28 -0300

    Moving platform animated

--- Scenes/MovingPlatform/MovingPlatform.gd
+++ Scenes/MovingPlatform/MovingPlatform.gd
@@ -1,8 +1,63 @@
-extends PathFollow2D
+extends AnimatableBody2D
 
 
-@export var speed = 150.0
+class TargetDistanceTime:
+	
+	var _position_to: Vector2
+	var _time: float
+	
+	# Constructor
+	func _init(position_from: Vector2, position_to: Vector2, speed: float):
+		_time = position_from.distance_to(position_to) / speed
+		_position_to = position_to
+		
+	
+	func _to_string() -> String:
+		return "PositionTo: %s, Time: %f" % [_position_to, _time]
+		
+
+@export var targets: Array[Marker2D]
+@export var speed: float = 150.0
 
 
-func _physics_process(delta: float) -> void:
-	progress += speed * delta
+var _tween: Tween
+var _target_points: Array[TargetDistanceTime]
+
+
+func _ready() -> void:
+	if targets.size() < 2:
+		queue_free()
+	else:
+		global_position = targets[0].global_position
+		setup_points()
+		set_moving()
+		
+		
+func _exit_tree() -> void:
+	if _tween:
+		_tween.kill()
+		
+
+func setup_points():
+	for i in range(len(targets) - 1):
+		var np: TargetDistanceTime = TargetDistanceTime.new(
+			targets[i].global_position,
+			targets[i + 1].global_position,
+			speed
+		)
+		_target_points.append(np)
+		
+	var np: TargetDistanceTime = TargetDistanceTime.new(
+		targets[targets.size() - 1].global_position,
+		targets[0].global_position,
+		speed
+	)
+	_target_points.append(np)
+
+
+func set_moving() -> void:
+	_tween = get_tree().create_tween()
+	_tween.set_loops()
+	for tp in _target_points:
+		_tween.tween_property(self, "global_position", tp._position_to, tp._time)
+		_tween.tween_interval(0.05)
---
+++ Scenes/MovingPlatformPF/MovingPlatformPF.gd
@@ -0,0 +1,8 @@
+extends PathFollow2D
+
+
+@export var speed = 150.0
+
+
+func _physics_process(delta: float) -> void:
+	progress += speed * delta
